efefd4ed9e0fd4133a4b93dc2b79386b6d6ee8d6
Michael Caputo <mike@caputo.io>
2016-09-24 16:17:25 +0000

    Add small world to mock game ids

--- app/mock-game-ids.ts
+++ app/mock-game-ids.ts
@@ -1,1 +1,5 @@
-export const GAME_IDS = {"cv": 143986, "sherlock holmes consulting detective": 2511 }
+export const GAME_IDS = {
+  "cv": 143986,
+  "sherlock holmes consulting detective": 2511,
+  "small world": 40692
+}
